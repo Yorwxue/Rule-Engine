5138fa7f2657d71d51ec7bbb094bf46582716653
clliao <yorwxue@gmail.com>
2021-04-06 16:27:43 +0800

    add function to get time of current

--- utils/timeFilter.py
+++ utils/timeFilter.py
@@ -2,20 +2,26 @@ import datetime
 from dateutil.relativedelta import relativedelta
 
 
+def getNow():
+    _out_date_ = datetime.datetime.now()
+    outDateString = datetime.datetime.strftime(_out_date_, "%Y%m%d%H%M%S")
+    return outDateString
+
+
 def getStartTime(InDateString, months=0, days=0, hours=0, minutes=0, seconds=0):
     """
     Computer the start time given deadline and period
-    :param InDateString: deadline
+    :param InDateString: deadline in string format
     :param months: number of months in period
     :param hours: number of hours in period
     :param minutes: number of minutes in period
     :return: start time
     """
-    _in_date_ = datetime.datetime.strptime(InDateString, "%Y%m%d%H%M%S")
+    _in_date_ = datetime.datetime.strptime(str(InDateString), "%Y%m%d%H%M%S")
     period = datetime.timedelta(days=days, hours=hours, minutes=minutes, seconds=seconds)
     _out_date_ = _in_date_ - period - relativedelta(months=months)
     outDateString = datetime.datetime.strftime(_out_date_, "%Y%m%d%H%M%S")
-    return outDateString
+    return int(outDateString)
 
 
 def mergeDateAndTime(dateString, timeString):
@@ -29,14 +35,16 @@ def mergeDateAndTime(dateString, timeString):
     _date_ = datetime.datetime.strftime(_date_, "%Y%m%d")
     _time_ = datetime.datetime.strptime(str(timeString), "%H%M%S")
     _time_ = datetime.datetime.strftime(_time_, "%H%M%S")
-    return _date_ + _time_
+    return int(_date_ + _time_)
 
 
 if __name__ == "__main__":
-    in_date = "2019/5/9"
-    in_time = "1530"
-    in_dateString = mergeDateAndTime(in_date, in_time)
-    # in_dateString = "20210406101101"
-    print(in_dateString)
-    out_dateString = getStartTime(in_dateString, months=1, days=1, hours=3, minutes=30, seconds=30)
-    print(out_dateString)
+    # in_date = "2019/5/9"
+    # in_time = "1530"
+    # in_dateString = mergeDateAndTime(in_date, in_time)
+    # # in_dateString = "20210406101101"
+    # print(in_dateString)
+    # out_dateString = getStartTime(in_dateString, months=1, days=1, hours=3, minutes=30, seconds=30)
+    # print(out_dateString)
+
+    print(getNow())
