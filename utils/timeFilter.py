e2ff89b37cee1d3abc2ae9c85b0bcfbc54c23b97
clliao <yorwxue@gmail.com>
2021-04-07 14:44:17 +0800

    implement timeDelta functions

--- utils/timeFilter.py
+++ utils/timeFilter.py
@@ -37,7 +37,34 @@ def mergeDateAndTime(dateString, timeString):
     return int(_date_ + _time_)
 
 
+def getDateTimeDelta(bDate, aDate):
+    """
+
+    :param bDate: before in YYYYmmddHHMMSS format
+    :param aDate: after in YYYYmmddHHMMSS format
+    :return: time delta in seconds
+    """
+    before = datetime.datetime.strptime(str(bDate), "%Y%m%d%H%M%S")
+    after = datetime.datetime.strptime(str(aDate), "%Y%m%d%H%M%S")
+    delta = after - before
+    return int(delta.total_seconds())
+
+
+def timeDelta2Seconds(d, H, M, S):
+    """
+    Calculate number of seconds for given period
+    :param d: days
+    :param H: hours
+    :param M: minutes
+    :param S: seconds
+    :return: time delta in seconds
+    """
+    delta = datetime.timedelta(days=d, hours=H, minutes=M, seconds=S)
+    return int(delta.total_seconds())
+
+
 if __name__ == "__main__":
+    # print(getNow())
     # in_date = "2019/5/9"
     # in_time = "1530"
     # in_dateString = mergeDateAndTime(in_date, in_time)
@@ -46,4 +73,7 @@ if __name__ == "__main__":
     # out_dateString = getStartTime(in_dateString, months=1, days=1, hours=3, minutes=30, seconds=30)
     # print(out_dateString)
 
-    print(getNow())
+    print(getDateTimeDelta("20181231235959", "20200101000001"))
+    print(timeDelta2Seconds(d=1, H=1, M=1, S=1))
+
+    pass
